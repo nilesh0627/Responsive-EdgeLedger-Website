5860b00aefd71ee5231d5a82ef67ac2d24f4ecec
Nilesh Mishra <nilesh0627@gmail.com>
2021-01-07 20:08:01 +0530

    responsive design for mobile and tablet devices

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// Google Map
+let map;
+function initMap() {
+  map = new google.maps.Map(document.querySelector(".map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+
+// Smooth Scrolling
+$('.navbar a, .btn').on('click', function(event){
+    if(this.hash!==''){
+        event.preventDefault()
+        const hash=this.hash
+        $('html,body').animate(
+            {
+            scrollTop:$(hash).offset().top-100
+        },800);
+    }
+});
+
+
+//sticky Navbar transparent background 
+
+window.addEventListener('scroll',function(){
+    if(window.scrollY>150){
+        document.querySelector('.navbar').style.opacity=0.9
+    }
+    else{
+        document.querySelector('.navbar').style.opacity=1
+    }
+});
